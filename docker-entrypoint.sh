e12d06b7b724b7d02e36a3aed55607cf0a0b3510
hhy98741 <gitcommitter@eskietech.com>
2026-02-02 19:50:54 -0800

    docker alias

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+alias cld='claude --continue'
+alias cldyolo='claude --dangerously-skip-permissions'
+
 # Clear configurations to avoid caching issues in development
 echo "Clearing configurations..."
 php artisan config:clear
