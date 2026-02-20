98915d12b93cbb932282f93c02e6d64e038eebe3
hhy98741 <gitcommitter@eskietech.com>
2026-02-20 00:02:20 -0800

    potentially set container volume

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,5 +8,10 @@ php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
+# Ensure PulseAudio is available then set the volume of audio playback
+if command -v pactl >/dev/null 2>&1; then
+    pactl set-sink-volume @DEFAULT_SINK@ 67%
+fi
+
 # Execute the original command
 exec "$@"
